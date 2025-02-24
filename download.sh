55f84cf148b1708667ef24875f505e7435c519a5
wlc952 <wlc@zju.edu.cn>
2025-02-24 20:01:38 +0800

    0

--- download.sh
+++ download.sh
@@ -1,4 +1,3 @@
-mkdir test
-# python3 -m dfss --url=open@sophgo.com:/ext_model_information/LLM/LLM-TPU/deepseek-r1-distill-qwen-7b.zip
-# unzip deepseek-r1-distill-qwen-7b.zip
-# rm -rf deepseek-r1-distill-qwen-7b.zip
+python3 -m dfss --url=open@sophgo.com:/ext_model_information/LLM/LLM-TPU/deepseek-r1-distill-qwen-7b.zip
+unzip deepseek-r1-distill-qwen-7b.zip
+rm -rf deepseek-r1-distill-qwen-7b.zip
