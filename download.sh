8c5a1d51ce0f5a3bb766c4913b560b40a1c147ed
王连超 <wlc@zju.edu.cn>
2025-03-15 10:15:27 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,3 +1,3 @@
-python3 -m dfss --url=open@sophgo.com:/ext_model_information/LLM/LLM-TPU/deepseek-r1-distill-qwen-7b.zip
-unzip deepseek-r1-distill-qwen-7b.zip
-rm -rf deepseek-r1-distill-qwen-7b.zip
+# python3 -m dfss --url=open@sophgo.com:/ext_model_information/LLM/LLM-TPU/deepseek-r1-distill-qwen-7b.zip
+# unzip deepseek-r1-distill-qwen-7b.zip
+# rm -rf deepseek-r1-distill-qwen-7b.zip
